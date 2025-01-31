6c8e63b501640733e61e9fc06fafb28fe164fdd9
lavishsinghal12 <133774395+lavishsinghal12@users.noreply.github.com>
2025-01-31 18:01:08 +0530

    Add files via upload

---
+++ mahashivratri.js
@@ -0,0 +1,62 @@
+// slider information section second
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+}
+
+document.addEventListener('click',activate,false);
+
+
+// banner slider
+let currentIndex = 0;
+
+const slides = document.querySelectorAll('.slide1');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  const slider = document.querySelector('.sliderbanner');
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  showSlide(currentIndex);
+}
+
+// Auto slide every 3 seconds
+setInterval(nextSlide, 3000);
+
+// Initialize the first slide
+showSlide(currentIndex);
+
+
+
+// scroll to top 
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
